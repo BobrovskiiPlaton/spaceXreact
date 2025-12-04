6f76b81eb601a4d3b7fa7400f32efa85ff95a7c6
KreKer_ <platon.bob@gmail.com>
2025-12-04 15:06:56 +0300

    Revert "help"
    This reverts commit e14cc9d0ffe5b34a180e206786b118bc7b3259cd.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,55 +1,24 @@
 import {LaunchList} from "./launchList";
 import {Map} from "./map";
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState} from "react";
 import {SpaceX} from "../api/spacex";
 
 function App(){
-    const [launches, setLaunches] = useState([]);
-    const [launchpads, setLaunchpads] = useState([]);
-    const mapFunctionsRef = useRef(null);
-    const spacex = new SpaceX();
 
+    const [launches, setLaunches] = useState([]);
+    const spacex = new SpaceX();
     useEffect(()=>{
         spacex.launches().then(data =>{
-            setLaunches(data);
-        })
-        spacex.launchpads().then(data =>{
-            setLaunchpads(data);
+            setLaunches(data)
         })
     },[])
 
-    useEffect(() => {
-        if (mapFunctionsRef.current?.showMap && launchpads.length > 0) {
-            mapFunctionsRef.current.showMap(launchpads);
-        }
-    }, [launchpads]);
-
-    const onMapReady = (mapFunctions) => {
-        mapFunctionsRef.current = mapFunctions;
-    }
-
-    const handleLaunchInteraction = (launchpadId) => {
-    if (!mapFunctionsRef.current?.showLaunchpad) return;
-    
-    if (launchpadId) {
-        mapFunctionsRef.current.showLaunchpad(launchpadId);
-    } else {
-        if (mapFunctionsRef.current?.reset) {
-            mapFunctionsRef.current.reset();
-        }
-    }
-};
-
-return(
-    <main className='main'>
-        <LaunchList 
-            launches={launches} 
-            onHoverEnter={(id) => handleLaunchInteraction(id)}
-            onHoverEnd={() => handleLaunchInteraction(null)}
-        />
-        <Map launchpads={launchpads} onMapReady={onMapReady}/>
-    </main>
-)
+    return(
+        <main className='main'>
+            <LaunchList launches = {launches}/>
+            <Map/>
+        </main>
+    )
 }
 
 export {App};
--- src/components/launchList.jsx
+++ src/components/launchList.jsx
@@ -1,33 +1,15 @@
-import React from 'react';
-
 function LaunchList(props) {
-    const [selected, setSelected] = React.useState([]);
 
-    const handleClick = (launch) => {
-        if (selected.includes(launch.id)) {
-            setSelected(selected.filter(id => id !== launch.id));
-            props.onHoverEnd();
-        } else {
-            setSelected([...selected, launch.id]);
-            props.onHoverEnter(launch.launchpad);
-        }
-    };
-    
     return (
         <aside className="aside" id="launchesContainer">
             <h3>Launches</h3>
             <div id="listContainer">
+
+
                 <ul>
-                    {props.launches.map(launch => (
-                        <li key={launch.id}>
-                            <button
-                                className={selected.includes(launch.id) ? 'selected' : ''}
-                                onClick={() => handleClick(launch)}
-                            >
-                                {launch.name}
-                            </button>
-                        </li>
-                    ))}
+                    {props.launches.map(launch => {
+                        return <li key={launch.id}>{launch.name}</li>
+                    })}
                 </ul>
             </div>
         </aside>
